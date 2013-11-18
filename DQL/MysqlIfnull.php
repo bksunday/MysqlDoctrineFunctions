4568a38b1e84513c25e29cb196c348f15157235d
Anthony Servedio <a.servedio@technotraffic.com>
2013-11-18 14:43:59 -0500

    Added ifnull (from https://github.com/beberlei/DoctrineExtensions/tree/master/lib/DoctrineExtensions/Query/Mysql)

---
+++ DQL/MysqlIfnull.php
@@ -0,0 +1,38 @@
+<?php
+namespace Mapado\MysqlDoctrineFunctions\DQL;
+
+use \Doctrine\ORM\Query\AST\Functions\FunctionNode,
+    \Doctrine\ORM\Query\Lexer;
+
+/**
+ * Usage: IFNULL(expr1, expr2)
+ * 
+ * If expr1 is not NULL, IFNULL() returns expr1; otherwise it returns expr2.
+ * IFNULL() returns a numeric or string value, depending on the context in
+ * which it is used.
+ * 
+ * @author  Andrew Mackrodt <andrew@ajmm.org>
+ * @version 2011.06.12
+ */
+class MysqlIfnull extends FunctionNode
+{
+    private $expr1;
+    private $expr2;
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->expr1 = $parser->ArithmeticExpression();
+        $parser->match(Lexer::T_COMMA);
+        $this->expr2 = $parser->ArithmeticExpression();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'IFNULL('
+            .$sqlWalker->walkArithmeticPrimary($this->expr1). ', '
+            .$sqlWalker->walkArithmeticPrimary($this->expr2).')';
+    }
+}
